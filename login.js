5143d5de84bf6f0d3420adf1ee508b570dd9ce4c
emily <emilydu@live.ca>
2024-02-02 21:49:18 -0500

    connecting the pages

---
+++ login.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the form
+document.getElementById('login-submit').addEventListener('click', function(event) {
+  event.preventDefault();
+  window.location.href = 'calendar-list.html';  
+});
