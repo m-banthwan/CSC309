853ac03f11f20e9aecd63bee7da42de8a79847a2
emily <emilydu@live.ca>
2024-02-02 22:45:42 -0500

    correct New Account page

---
+++ newAccount.js
@@ -0,0 +1,5 @@
+// CREATE ACCOUNT: Reveals success message
+document.getElementById('new-account-submit').addEventListener('click', function(event) { //shold be submit but changed for P1
+  var successText = document.getElementById('new-account-hidden'); 
+  successText.style.color = 'black';
+}); 
