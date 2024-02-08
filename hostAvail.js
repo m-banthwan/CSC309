a1c6d161c1e4dcf8019c21014d13db1c8d7b8c2b
Mugdha Banthwan <113299553+m-banthwan@users.noreply.github.com>
2024-02-08 00:14:51 -0500

    Host Availability Calendar features

---
+++ hostAvail.js
@@ -0,0 +1,18 @@
+// Change color of time slot when chosen
+function changeColor1()
+{
+    var x = document.getElementById("busy");
+    x.bgColor = "#0B032D";            
+}
+
+function changeColor2()
+{
+    var x = document.getElementById("moderate");
+    x.bgColor = "#843B62";            
+}
+
+function changeColor3()
+{
+    var x = document.getElementById("free");
+    x.bgColor = "#FFB997";            
+}
